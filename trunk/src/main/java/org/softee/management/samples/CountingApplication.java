0f42986ffae7001d29004bb42c888a13290296ed
morten.hattesen@gmail.com <morten.hattesen@gmail.com@a6ce861c-9d6e-b028-0053-343f0fc94fad>
2011-07-10 05:50:43 +0000

    comment

--- trunk/src/main/java/org/softee/management/samples/CountingApplication.java
+++ trunk/src/main/java/org/softee/management/samples/CountingApplication.java
@@ -1,6 +1,5 @@
 package org.softee.management.samples;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.management.MalformedObjectNameException;
@@ -15,27 +14,28 @@ import org.softee.management.exception.ManagementException;
 import org.softee.management.helper.MBeanRegistration;
 
 /**
- * pojo-mbean demonstration sample application.<p>
+ * pojo-mbean sample application.<p>
  *
  * The application runs until manually stopped.<p>
  *
- * @ManagedAttribute methods will be called from multiple threads (application as well as MBean server)
+ * {@code @ManagedAttribute} methods will be called from multiple threads (application as well as MBean server)
  * and appropriate thread safety precautions must therefore be taken.<br>
  * These may include:
  * <ul>
  * <li>Make the methods synchronized</li>
  * <li>Include synchronized blocks around critical sections of the method</li>
  * <li>Make the fields volatile</li>
- * <li>Use threadsafe types, and use them in a threadsafe manner (used in this example)</li>
+ * <li>Use concurrent types, and use them in a thread safe manner (used in this example)</li>
  * </ul>
  *
- *  For more information about threadsafety, I hightly recommend the books "Effective Java", and "Concurrency in Practice".
+ *  For more information about concurrency and thread safety, I highly recommend the books
+ *  "Effective Java", and "Concurrency in Practice".
  *
  * @author morten.hattesen@gmail.com
  */
 @MBean(objectName = "org.softee:type=Demo,name=CountingApplication")
 @Description("This Java application shows what is required expose a RW int property as an MBean")
-public class CountingApplication implements Callable<Object> {
+public class CountingApplication {
     /**
      * The counter that is made visible to both application and Management Agent (JConsole / JMX console)
      */
@@ -44,16 +44,16 @@ public class CountingApplication implements Callable<Object> {
     public CountingApplication() throws ManagementException, MalformedObjectNameException {
         counter = new AtomicInteger();
         System.out.println("Registering MBean");
-        // in a "real" application, the registration instance would probably be saved in a field for subsequent unregistration
+        /* in a "real" application, the registration instance would probably be
+         * saved in a field for subsequent unregistration */
         new MBeanRegistration(this).register();
     }
 
     public static void main(String[] args) throws Exception {
-        new CountingApplication().call();
+        new CountingApplication().run();
     }
 
-    @Override
-    public Object call() throws Exception {
+    public Object run() throws Exception {
         for (;;) {
             System.out.println("counter = " + getCounter());
             incrementCounter(1);
@@ -71,7 +71,8 @@ public class CountingApplication implements Callable<Object> {
         this.counter.set(counter);
     }
 
-    @ManagedOperation(Impact.ACTION_INFO) @Description("Increments the counter by the requested amount and shows the resulting value")
+    @ManagedOperation(Impact.ACTION_INFO) // Impact may be omitted
+    @Description("Increments the counter by the requested amount and shows the resulting value")
     public int incrementCounter(
             @Parameter("amount") @Description("The amount to increment the counter with") int delta) {
         return counter.addAndGet(delta);
