55cf3e6e818990c65f0c9ca4352e5970e4c68d21
morten.hattesen@gmail.com <morten.hattesen@gmail.com@a6ce861c-9d6e-b028-0053-343f0fc94fad>
2011-07-13 19:20:00 +0000

    JavaDoc

--- trunk/src/main/java/org/softee/util/Objects.java
+++ trunk/src/main/java/org/softee/util/Objects.java
@@ -5,16 +5,29 @@ public class Objects {
     private Objects() {
     }
 
-    public static <T> T firstNotNull(T... all) {
-        for (T element : all) {
+    /**
+     * @param <E> The type of elements
+     * @param all any number of elements
+     * @return the first element of {@code all} that is not null
+     * @throws NullPointerException if no element is not-null
+     */
+    public static <E> E firstNotNull(E... all) {
+        for (E element : all) {
             if (element != null) {
                 return element;
             }
         }
-        throw new IllegalArgumentException("All null arguments");
+        throw new NullPointerException("All null arguments");
     }
 
-    public static <T> T notNull(T obj) {
+    /**
+     * An assertion method that makes null validation more fluent
+     * @param <E> The type of elements
+     * @param obj an Object
+     * @return {@code obj}
+     * @throws NullPointerException if {@code obj} is null
+     */
+    public static <E> E notNull(E obj) {
         if (obj == null) {
             throw new NullPointerException();
         }
